32756edb5d4d194e81698edbffb038101aea3d36
RubikEnglish <71373145+RubikEnglish@users.noreply.github.com>
2020-10-02 14:24:59 -0500

    commit 1

---
+++ app.js
@@ -0,0 +1,140 @@
+/*document.write ("<h1>Hello World</h1>");
+console.log({"username": "Jhon", "score": "30"});
+var name = "Jhon";
+let lastName ="Vera";
+lastName = "Olmos";
+
+const PI = 3.1415;
+
+console.log(name)
+console.log(lastName)
+console.log(PI)
+
+var result = name +" "+ lastName
+
+console.log(result);
+
+let numberOne = 100;
+let numberTWO = 50+50;
+
+let resultOne = numberOne == numberTWO;
+
+console.log(resultOne)
+
+var holeName = "Jhon Olmos"
+
+resultTwo = holeName == result
+
+console.log(resultTwo)
+
+document.write (resultTwo)*/
+
+/*Validación de cuentas
+
+let passwordDB = "pepe123"
+let input = "pepe"
+
+let result = input == passwordDB;
+
+console.log (result)
+
+if (result == true){
+    document.write ("Login correcto")
+} else {
+    document.write ("Login incorrecto")
+}*/
+
+/*VARIABLE 3
+
+let score =70;
+
+if (score > 30){
+    console.log("you need to practice more");
+} else if (score > 15){
+    console.log("Estas mejorando");
+}
+else {
+    console.log ("You need to follow this tutorial");
+}*/
+
+/* SWITCH 
+
+let typeCard = ")))";
+
+switch(typeCard) {
+    case "Debid Card":
+        console.log("This is a debid card");
+        break;
+    case "Credit Card":
+        console.log("This is a credit card");
+        break;
+    default:
+        console.log("No Card");
+}*/
+
+//LOOP
+
+/*let count =50;
+
+while(count > 0){
+    document.write (count);
+    console.log (count);
+    count = count -1;
+}*/
+
+
+/*let count =1;
+
+while(count < 50){
+    document.write (count);
+    console.log (count);
+    count++;
+}*/
+
+/*let names = [1, 2, 3];
+
+for (let i= names.length - 1; i >= 0; i--) {
+    document.write(names [i]);
+    console.log(names[i]);
+}*/
+
+//     FUNCIONES
+
+
+
+function greeting (name) {
+    document.write ("Hello "+ name)
+    }
+
+greeting("jhon");
+
+greeting("marcus");
+
+
+function add (n1,n2){
+    console.log(n1 + n2);
+}
+
+add(34, 500);
+
+function add (n1,n2){
+    console.log(n1 + n2);
+}
+
+function addnames (name1, name2){
+    console.log (name1+name2)
+}
+addnames("karla", "Lalo")
+
+document.write(Math.round (Math.random()*10)) ;
+
+
+
+
+
+
+
+
+
+
+
